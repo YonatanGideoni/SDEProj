92a3911a871d5c19d62c91e5ca2be0f3113db8f9
Dulhan Jayalath <dhj1g19@soton.ac.uk>
2023-03-03 13:17:57 +0000

    First working bayesian filter implementation

--- iwp_bayesian_filter.py
+++ iwp_bayesian_filter.py
@@ -5,9 +5,10 @@ import math
 # If everything is 1-dimensional, Magnani et al. use of H in algorithm is correct
 # We don't know if we set 1st or 2nd element should be 1 for extracting measurement.
 # Most likely second thing - as this is what we directly observe - the "measurement"
+# Francisco: H indexes into the LAST dimension!
 def calc_H_mat(q: int):
-    H = np.zeros((1, q + 1)) # Assume paper is wrong about (1, q) - must be (1, q + 1) to work!
-    H[0, 1] = 1
+    H = np.zeros((q + 1, q + 1)) # Assume paper is wrong about (1, q) - must be (1, q + 1) to work!
+    H[:, -1] = 1
     return H
 
 def calc_A_mat(dt: float, q: int):
@@ -30,17 +31,14 @@ def calc_Q_mat(dt: float, q: int, sigma = 1.0):
 
 # TODO - make this work for generic dts and not just a set one
 def ode_filter(func: callable, init_mean: np.ndarray, init_cov: np.ndarray, dt: float, n_steps: int):
-    dim = init_mean.shape[0] # dim = q + 1
-    # print(dim)
 
-    # if dim == ():
-    #     dim = 1
-    # else:
-    #     assert (dim, dim) == init_cov.shape, 'Error - init mean and covariance have a shape mismatch!'
+    dim = init_mean.shape[0] # dim = q + 1
+
+    assert (dim, dim) == init_cov.shape, 'Error - init mean and covariance have a shape mismatch!'
 
     A = calc_A_mat(dt, q=dim - 1) # (q + 1, q + 1)
     Q = calc_Q_mat(dt, q=dim - 1) # (q + 1, q + 1)
-    H = calc_H_mat(q=dim - 1)  # (1, q)
+    H = calc_H_mat(q=dim - 1)  # (q + 1, q + 1) -> NOTE: Deviates from definition in paper
 
     t = 0
 
@@ -51,24 +49,23 @@ def ode_filter(func: callable, init_mean: np.ndarray, init_cov: np.ndarray, dt: 
 
     prev_mean, prev_cov = init_mean, init_cov
     for step in range(1, n_steps):
+
         # might want to consider changing integration schemes, eg. to leapfrog. Not sure how rigorous that would be
         t += dt
 
-        print(A.shape, prev_mean.shape)
         approx_mean = A @ prev_mean # (q + 1, 1) = (q + 1, q + 1) x (q + 1, 1)
         approx_cov = A @ prev_cov @ A.T + Q # (q + 1, q + 1) = (q + 1, q + 1) x (q + 1, q + 1) x (q + 1, q + 1)
 
         obs = func(t, approx_mean)  # (q + 1, 1) TODO - is this literally just the derivative, AKA x'=f(t, x)?
         mean_error = obs - H @ prev_mean # (q + 1, 1) = (q + 1, 1) - (1, 1) (FIXME: ???)
-        # What's the issue? Error should be different for EACH! so instead of (1, q + 1) x (q + 1, 1) = (1, 1)
+        # What's the issue? Error should be different for EACH vector element! so instead of (1, q + 1) x (q + 1, 1) = (1, 1)
         # we want a result that's (q + 1, 1) = (x, y) x (q + 1, 1) hence H should be (q + 1, q + 1)
 
         # FIXME: 1x1 for 1-dim case?
         S_mat = H @ approx_cov @ H.T  # (1, 1) = (1, q + 1) x (q + 1, q + 1) x (q + 1, 1) TODO - understand meaning
         # TODO is this numerically stable? Might be able to improve stability by changing the inv to a left-solve
-        rel_unc = approx_cov @ H.T @ np.linalg.inv(S_mat) # (q + 1, 1) = (q + 1, q + 1) x (q + 1, 1) x (1, 1)
+        rel_unc = approx_cov @ H.T @ np.linalg.pinv(S_mat) # (q + 1, 1) = (q + 1, q + 1) x (q + 1, 1) x (1, 1)
 
-        print(approx_mean.shape, rel_unc.shape, mean_error.shape)
         mean = approx_mean + rel_unc @ mean_error # (q + 1, 1) + (q + 1, 1) x (q + 1, 1) (FIXME: can't multiply this!)
         cov = approx_cov - rel_unc @ S_mat @ rel_unc.T
 
@@ -81,32 +78,42 @@ def ode_filter(func: callable, init_mean: np.ndarray, init_cov: np.ndarray, dt: 
 # Should be at least 2 - even for 1-dimensional case as we need original + derivative
 if __name__ == "__main__":
 
-    def wiener_1d(t: float, mean: float):
-        return np.random.normal(loc=mean, scale=1.0)
-    
-    def simple_ode(t: float, mean: float):
-        return mean + t
-
     # Time step size should be 0.5 for this.
     def negative_exponential_ode(t: float, mean: float):
         return -mean
     
+    order = 1
+
+    # Initialise init_mean as defined in Magnani et al.
+    init_mean = np.zeros((order + 1, 1), dtype=np.float32)
+    init_mean[0, 0] = 1.0 # x_0
+    init_mean[1, 0] = negative_exponential_ode(0.0, init_mean[0, 0]) # f(0, x_0)
+
+    # Initialise init_cov as defined in Magnani et al. (TODO: Check correctness)
+    init_cov = np.eye(order + 1, dtype=np.float32)
+
+    dt = 0.5
+    n_steps = 24
+    
     means, covs = ode_filter(
         func=negative_exponential_ode,
-        init_mean=np.zeros((2, 1), dtype=np.float32), # Should be defined to be the order we want to work with. Check p7 notes on intialisation.
-        init_cov=np.eye(2, dtype=np.float32),
-        dt=0.5,
-        n_steps=24)
-
-    print(means)
-    print(covs)
+        init_mean=init_mean, # Should be defined to be the order we want to work with. Check p7 notes on intialisation.
+        init_cov=init_cov,
+        dt=dt,
+        n_steps=n_steps)
 
     import scipy
 
-    # init = np.array(0, dtype=np.float32).reshape(1)
-    init = np.zeros((2, 1), dtype=np.float32)
-    res = scipy.integrate.solve_ivp(negative_exponential_ode, (0.0, 12.0), init, rtol=1e-5, atol=1e-5, method='RK45')
+    init = init_mean.reshape(order + 1,)
+    res = scipy.integrate.solve_ivp(negative_exponential_ode, (0.0, (n_steps - 1) * dt), init, rtol=1e-5, atol=1e-5, method='RK45')
     print(res)
+
+    import matplotlib.pyplot as plt
+
+    plt.plot([dt * i for i in range(n_steps)], means[:, 0], label="Bayesian filter")
+    plt.plot(res["t"], res["y"][0], label="Scipy RK45")
+    plt.legend()
+    plt.show()
 
 # Use L1 or L2 over function space to measure closeness
 # Also plot and examine functions
@@ -114,8 +121,3 @@ if __name__ == "__main__":
 # Also, assuming they converge,
 # we can plot error w.r.t. GT vs # iterations and plot RK4 vs our method
 # and hope our error drops faster.
-
-# Questions for Francisco:
-# - How to choose q?
-# - How to do multi-dimensional?
-# - How to choose prior?
